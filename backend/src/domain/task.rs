ca6e873dd195d45329b479d8d3ce7513f8c86beb
diegobraga92 <diego.braga92@outlook.com>
2026-02-27 05:59:05 -0500

    Starting to rewrite code

--- backend/src/domain/task.rs
+++ backend/src/domain/task.rs
@@ -1,11 +1,17 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+#[derive(Serialize, Deserialize, Debug)]
+pub enum TaskType {
+    Regular = 0,
+    Daily = 1,
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Task {
     pub id: Uuid,
     pub list_id: Uuid,
     pub title: String,
-    pub task_type: String, // "regular" | "daily"
-    pub deleted: bool,
+    pub task_type: TaskType,
+    pub checked: bool,
 }
