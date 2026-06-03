5ace6205c5a658638a5817d0d4afb85d48872e0d
diegobraga92 <diego.braga92@outlook.com>
2026-06-03 08:30:55 -0300

    Updating AGP

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file for PudimProductivity Android app.
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
 }
