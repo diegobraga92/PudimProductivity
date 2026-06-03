f31ee0f09ceb02f91c425006265cefd75364ea26
diegobraga92 <diego.braga92@outlook.com>
2026-06-03 08:31:58 -0300

    Updating Kotlin

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,5 +1,5 @@
 // Top-level build file for PudimProductivity Android app.
 plugins {
     id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.20" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
 }
