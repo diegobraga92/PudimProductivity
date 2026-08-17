5798b2fe811ea1f9d9f683e1c042c73c3e32a03f
diegobraga92 <diego.braga92@outlook.com>
2026-08-17 12:50:16 -0300

    Rewritting CI code

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file for PudimProductivity Android app.
 plugins {
-    id("com.android.application") version "9.1.1" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.21" apply false
+    id("com.android.application") version "9.3.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
     // OWASP dependency-check: vulnerability scanning of Android/Java dependencies.
     // Task: ./gradlew dependencyCheckAnalyze
     // 12.2.2 widens the NVD reference URL column (fixes the CVE-2026-6785/6786
