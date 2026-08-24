ad57c29caac3d485c985a2bdd703b07bfc43ab2f
diegobraga92 <diego.braga92@outlook.com>
2026-08-24 20:22:59 -0300

    Fixed mobile icon

--- mobile/app/src/main/java/com/pudimproductivity/fcm/PudimFirebaseMessagingService.kt
+++ mobile/app/src/main/java/com/pudimproductivity/fcm/PudimFirebaseMessagingService.kt
@@ -27,7 +27,7 @@ class PudimFirebaseMessagingService : FirebaseMessagingService() {
 
         createChannel()
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(title)
             .setContentText(body)
             .setAutoCancel(true)
